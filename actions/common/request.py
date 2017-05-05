691847b931f2c50afb503c6880a0648d8ff7db03
bigmstone <matthew99@gmail.com>
2017-05-04 22:59:55 -0500

    Initial commit

---
+++ actions/common/request.py
@@ -0,0 +1,23 @@
+"""Common file for handling requests.
+"""
+import requests
+from common.constants import ADDRESS
+
+
+def get_location(part):
+    """ Get the current location of a part.
+    """
+    request = requests.get('http://%s/v1/arm/%s' % (ADDRESS, part))
+
+    return request.json()
+
+
+def set_location(part, location):
+    """ Get the current location of a part.
+    """
+    request = requests.put(
+        'http://%s/v1/arm/%s' % (ADDRESS, part),
+        json={'location': location}
+    )
+
+    return request.json()
